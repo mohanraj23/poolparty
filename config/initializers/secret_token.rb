72288ea3b12af1f49f72fe084f1fe3b458fc6b36
mohanraj23 <smmohanraj23@gmail.com>
2013-08-17 02:42:54 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Poolparty::Application.config.secret_token = 'd7f71fe2cfb5d7edfced4be31af8338ee31fe556a78b7e5a683652a6adb940e4174cea23bba9e2fcc9628bcfac516b46c8ca729bef2a0a7ff053f62552f81571'
